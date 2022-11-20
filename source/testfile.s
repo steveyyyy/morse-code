d43690ce8d5dfa1a2ee270f40c9875e236dba5c2
Ladina Morf <ladina.morf@gmail.com>
2022-11-20 17:41:26 +0100

    added conversion of lower case letters and question when to do morse stuff

--- source/testfile.s
+++ source/testfile.s
@@ -1,6 +1,6 @@
 .section .data
 morseString:
-.asciz "sos"
+.asciz "sos"                       @ each letter is 8-bits (= 1 Byte) -> can use byte loader; .asciz := string, which ends with NULL
 
 .section .init  @ nicht sicher ob notwendig, CPUlator wollte das so
 .globl _start
@@ -18,19 +18,29 @@ main:
     mov r1,#1
     lsl r1,#21
 
+
 MainLoop:
     ldr r10, =morseString
     mov r9,#0                        @ counter of iterations
-    morseLetterLoop:                 @ while loop
+    convertToUpperCase:              @ while loop
         add r8, r9, r10
         ldrb r0, [r8]                @ load current letter in r0
-        cmp r0, #0                   @ check if value is zero -> break
+        cmp r0, #0                   @ check if value is null -> break
         beq endOfString
-        sub r0,#32                   @ morse here; for test convert to uppercase
+        cmp r0,#97                   @ check if < 97; ASCII boundary for lower case letter
+        blt loopIncrement
+        cmp r0, #122                 @ check if > 122; ASCII boundary for lower case letter
+        bgt loopIncrement
+
+        @ capitalize if lowercase letter
+        sub r0,#32                   @ convert to uppercase
         strb r0,[r8]                 @ save back
 
-        add r9, #1                   @ increment offset
-    b morseLetterLoop
+        @ TODO should we do the morse stuff here or in a separate loop after the conversion?
+
+        loopIncrement:
+            add r9, #1               @ increment offset
+    b convertToUpperCase
 
 endOfString:
     nop @ infinite Loop
